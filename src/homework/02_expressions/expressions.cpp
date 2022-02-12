b0b5f0e775da72692d87eec0cfbe1c670525afbc
Adrian Mendez <adrianmendez3@g.austincc.edu>
2022-02-11 22:35:17 -0600

    HW 2 with test cases

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -8,7 +8,15 @@ int add_numbers(int num1, int num2)
 
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
+double get_sales_tax_amount(double tax_rate, double meal_amount)
+{
+	return tax_rate * meal_amount;
+}
 
+double get_tip_amount(double meal_account, double tip_rate)
+{
+	return meal_account * tip_rate;
+}
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,14 +1,46 @@
 ﻿//write include statements
+#include "expressions.h"
+#include<iostream>
 
 
 //write namespace using statement for cout
-
+using std::cout;
+using std::cin;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
+	double meal_amount;
+	double tip_rate;
+	double tip_amount;
+	double tax_amount;
+	double total;
+	cin >> meal_amount;
+	double get_sales_tax_amount(double meal_amount);
+	{
+		return tax_amount;
+	}
+	cin >> tip_rate;
+	double get_tip_amount(double meal_account, double tip_rate);
+	{
+		return tip_amount;
+	}
+
+	{
+	double tip_amount, tax_amount, meal_amount;
+	cout<<"Enter three numbers to add\n";
+	cin>>tip_amount>> tax_amount>> meal_amount;
+
+	double total = tip_amount + tax_amount + meal_amount;
+
+	cout<<"\n the total is: " << total;
+}
+//meal amount: 20
+//sales tax: 1.35
+//tip amount: 3
+//total: 24.35
 	
 
 	return 0;
