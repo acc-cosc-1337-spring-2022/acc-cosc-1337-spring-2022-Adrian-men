1fea5d3aab3095c32269f7a20da3755be31ab6bf
Adrian Mendez <adrianmendez3@g.austincc.edu>
2022-02-26 21:53:36 -0600

    HW 4 with Test Cases

--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,4 +1,7 @@
 //write include statements
+#include "dna.h"
+
+using std::cin; using std::cout;
 
 //write using statements
 
@@ -12,5 +15,62 @@ user enters a y or Y.
 */
 int main() 
 {
-	return 0;
+	//prompt_user();
+	run_menu();
+    return 0;
+}
+
+
+
+void prompt_user()
+{
+    auto cont = 'y';
+    do
+    {
+        cout<<"are you sure you want to exit?";
+        cin>>cont;
+    }
+    while(cont == 'n'); 
+}
+
+void display_menu()
+{
+    cout<<"Factorial\n";
+    cout<<"Greatest Common Divisor\n";
+    cout<<"Exit\n";
+}
+
+void run_menu()
+{
+    auto option = 0;
+    do
+    {
+        display_menu();
+        cout<<"Enter a number ";
+        cin>>option;
+
+        handle_menu_option(option);
+
+    }
+    while(option != 3);
+    
+}
+
+void handle_menu_option(int option)
+{
+    switch(option)
+    {
+    case 1:
+        cout<<"Factorial\n";
+        break;
+    case 2:
+        cout<<"Greatest Common Divisor\n";
+        break;
+    case 3:
+        cout<<"Exit\n";
+        break;
+    default:
+        cout<<"Invalid Option\n";
+    }
+
 }
