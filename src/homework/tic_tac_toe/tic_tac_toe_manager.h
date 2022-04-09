424b3b389453bb9700c468cb8cafa0a75a56b083
Adrian Mendez <adrianmendez3@g.austincc.edu>
2022-04-09 17:19:17 -0500

    Hw 8 with test cases

--- src/homework/tic_tac_toe/tic_tac_toe_manager.h
+++ src/homework/tic_tac_toe/tic_tac_toe_manager.h
@@ -1,1 +1,26 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+#include "tic_tac_toe.h"
+
+#ifndef TIC_TAC_TOE_MANAGER_H
+#define TIC_TAC_TOE_MANAGER_H
+
+class TicTacToeManager
+{
+friend std::ostream& operator<<(std::ostream& out, const TicTacToeManager& manager);
+
+public:
+    void save_game(TicTacToe b);
+    void get_winner_total(int& o, int& w, int& t);
+private:
+    std::vector<std::string>games;
+    int x_wins;
+    int o_wins;
+    int ties;
+    void update_winner_count(std::string winner);
+
+};
+
+#endif
